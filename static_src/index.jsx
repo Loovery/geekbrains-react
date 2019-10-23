ee1dab585b952498deee0b0fc99589d4cd533bd8
Grigorii Korepanov <grigoriikorepanov@gmail.com>
2019-10-23 17:13:56 +0500

    first commit

---
+++ static_src/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * Created by Korepanov Grigorii on 23.10.2019
+ * grigoriikorepanov@gmail.com
+ * https://t.me/loovery
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div />,
+  document.getElementById('root'),
+);
