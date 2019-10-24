042cd52ef18f0d114ce45a1faf32c730f0f4a44d
Grigorii Korepanov <loovery@Grigoriis-MacBook-Air.local>
2019-10-24 13:48:12 +0500

    add App jsx loaders and readme

---
+++ static_src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * Created by Korepanov Grigorii on 24.10.2019
+ * grigoriikorepanov@gmail.com
+ * https://t.me/loovery
+ */
+
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <></>
+  }
+}
